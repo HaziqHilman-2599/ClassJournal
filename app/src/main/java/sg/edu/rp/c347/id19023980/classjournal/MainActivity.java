d5ce56b41bb6c9a2279e9c29df106956734d0125
19023980 <19023980@myrp.edu.sg>
2021-04-27 12:30:49 +0800

    Problem Statement

--- app/src/main/java/sg/edu/rp/c347/id19023980/classjournal/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c347/id19023980/classjournal/MainActivity.java
@@ -2,10 +2,14 @@ package sg.edu.rp.c347.id19023980.classjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = this.findViewById(R.id.lvModules);
+
+        al = new ArrayList<Modules>();
+        al.add(new Modules("Web Services","C302"));
+        al.add(new Modules("Android Programming II","C347"));
+
+        aa = new ModulesAdapter(this,R.layout.row,al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("Code", al.get(position).getCode());
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/sg/edu/rp/c347/id19023980/classjournal/SecondActivity.java
@@ -0,0 +1,4 @@
+package sg.edu.rp.c347.id19023980.classjournal;
+
+public class SecondActivity {
+}
